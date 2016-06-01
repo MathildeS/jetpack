ef27274ada330457f850033fde3ed1ce1ae452b9
dereksmart <smart@automattic.com>
2016-06-01 14:23:44 -0400

    Notices: Trigger the welcome/jumpstart notice based on the Jumpstart Actions.
    If Jumpstart is either skipped or activated, we can trust that this is a first-time connection.  This commit displays specific text after jumpstart welcoming the user and introducing them to the new Jetpack.

--- _inc/client/state/jetpack-notices/reducer.js
+++ _inc/client/state/jetpack-notices/reducer.js
@@ -14,14 +14,22 @@ import {
 	JETPACK_NOTICES_DISMISS_FAIL,
 	JETPACK_NOTICES_DISMISS_SUCCESS,
 	DISCONNECT_SITE_SUCCESS,
-	RESET_OPTIONS_SUCCESS
+	RESET_OPTIONS_SUCCESS,
+	JUMPSTART_ACTIVATE_SUCCESS,
+	JUMPSTART_SKIP
 } from 'state/action-types';
 import restApi from 'rest-api';
 
 const status = ( state = false , action ) => {
 	switch ( action.type ) {
-		case DISCONNECT_SITE_SUCCESS:
+		case DISCONNECT_SITE_SUCCESS :
 			return 'disconnected';
+
+		case JUMPSTART_ACTIVATE_SUCCESS :
+			return 'new_connection_jumpstart';
+
+		case JUMPSTART_SKIP :
+			return 'new_connection_no_jumpstart';
 
 		default:
 			return state;
